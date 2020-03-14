3ac1aa438c551c2194432266da893a2fc28b072b
Lukas <lukas@uzolas.com>
2020-03-14 19:59:10 +0100

    decay added

--- models/cnn_models.py
+++ models/cnn_models.py
@@ -8,7 +8,10 @@ from keras.models import Model, Sequential
 from keras.layers.advanced_activations import LeakyReLU
 from keras.optimizers import Adam
 
-def discriminator(input_dim=(28,28,1), depth=32, dropout=0.4, lr=0.00004):
+def calc_decay(lr, epoch=100):
+    return lr/epoch
+
+def discriminator(input_dim=(28,28,1), depth=64, dropout=0.4, lr=0.0004):
     model = Sequential()
 	
     model.add(Conv2D(int(depth/2), (3,3), strides=(2,2), padding='same', input_shape=input_dim))
@@ -21,10 +24,11 @@ def discriminator(input_dim=(28,28,1), depth=32, dropout=0.4, lr=0.00004):
     model.add(Dropout(dropout))
     model.add(Dense(1, activation='sigmoid'))
 	
-    model.compile(loss='binary_crossentropy', optimizer=Adam(lr=lr, beta_1=0.5), metrics=['accuracy'])
+    model.compile(loss='binary_crossentropy',
+                  optimizer=Adam(lr=lr, beta_1=0.5, decay=calc_decay(lr)), metrics=['accuracy'])
     return model
 
-def generator(input_dim=100, depth=32, dim=7):
+def generator(input_dim=100, depth=64, dim=7):
     model = Sequential()
     n_nodes = depth * 7 * 7
     model.add(Dense(n_nodes, input_dim=input_dim))
@@ -40,14 +44,14 @@ def generator(input_dim=100, depth=32, dim=7):
     model.add(Conv2D(1, (7,7), activation='tanh', padding='same'))
     return model
 
-def gan(discriminator, generator, gen_input_dim=100, lr=0.00002):
+def gan(discriminator, generator, gen_input_dim=100, lr=0.0002):
 	discriminator.trainable = False
 
 	model = Sequential()
 	model.add(generator)
 	model.add(discriminator)
 
-	opt = Adam(lr=lr, beta_1=0.5)
+	opt = Adam(lr=lr, beta_1=0.5, decay=calc_decay(lr))
 	model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
 
 	return model
